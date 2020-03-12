c609e40d77786d018be1a621999fd537814e98f9
Nick Anstoetz <nanstoetz@lcs.on.ca>
2020-03-11 20:27:11 -0400

    I made every player have to choose a number between 4-6 to decide how much player can play the game. I also made ze choose names for the players. finished #1

--- RollingStone/main.swift
+++ RollingStone/main.swift
@@ -8,5 +8,95 @@
 
 import Foundation
 
-print("Hello, World!")
+// Add functionality to the Card structure
+extension Card {
+    
+    // Computed property that returns true when the card is a face card (class work)
+    var isFaceCard: Bool {
+        if self.rank == .ace ||
+            self.rank == .king ||
+            self.rank == .queen ||
+            self.rank == .jack {
+                return true
+        } else {
+            return false
+        }
+    }
+}
 
+
+class RollingStone {
+    
+    //create the deck
+    var deck : Deck
+    
+    //creating the 6 users
+    var player1 : Hand
+    var player2 : Hand
+    var player3 : Hand
+    var player4 : Hand
+    var player5 : Hand
+    var player6 : Hand
+
+    //adding the middle to add cards to
+    var middle : Hand
+
+    //choosing the how many players should play
+    let minPlayernumber = 4
+    let maxPlayernumber = 6
+    init(){
+        player1 = Hand(description: "none")
+        player2 = Hand(description: "none")
+        player3 = Hand(description: "none")
+        player4 = Hand(description: "none")
+        player5 = Hand(description: "none")
+        player6 = Hand(description: "none")
+        
+        middle = Hand(description: "middle")
+        deck = Deck()
+        var possibleNumber = 0
+        while true{
+            print("Please Enter the number of player you want to play with")
+            guard let numberInput = readLine(), let playerCount = Int(numberInput) else {
+                continue
+            }
+            possibleNumber = playerCount
+            break
+        }
+        if possibleNumber == 4 {
+            print("Please Enter the name of four players you want to play with")
+            player1 = Hand(description: readLine()!)
+            player2 = Hand(description: readLine()!)
+            player3 = Hand(description: readLine()!)
+            player4 = Hand(description: readLine()!)
+            player5 = Hand(description: "none")
+            player6 = Hand(description: "none")
+
+        }
+        else if possibleNumber == 5 {
+            print("Please Enter the name of five players you want to play with")
+            player1 = Hand(description: readLine()!)
+            player2 = Hand(description: readLine()!)
+            player3 = Hand(description: readLine()!)
+            player4 = Hand(description: readLine()!)
+            player5 = Hand(description: readLine()!)
+            player6 = Hand(description: "none")
+
+         }
+        else if possibleNumber == 6 {
+            print("Please Enter the name of six players you want to play with")
+            player1 = Hand(description: readLine()!)
+            player2 = Hand(description: readLine()!)
+            player3 = Hand(description: readLine()!)
+            player4 = Hand(description: readLine()!)
+            player5 = Hand(description: readLine()!)
+            player6 = Hand(description: readLine()!)
+
+        } else{
+        print("restart and type number between 4-6")
+        }
+    }
+    
+
+}
+RollingStone()
